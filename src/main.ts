a927df22123eedb3ee97f375db44a33991303f09
UlrikeWerner <ulrike_werner@web.de>
2023-09-26 15:51:40 +0200

    task 4

--- src/main.ts
+++ src/main.ts
@@ -28,3 +28,14 @@ printStudent(anton);
 
 anton.gradeCertificate = ["A", 2, undefined, 3, 1, "B", undefined, 5];
 printStudent(anton);
+
+function printStudentList(studentList: Student[]): void {
+    studentList.forEach((student: Student) => printStudent(student));
+}
+
+let paul: Student = {firstName: "Paul", secoundName: "Peterson", age: 19, gradeCertificate: [1, "C", 5, undefined, "B", undefined]};
+let lisa: Student = {firstName: "Lisa", secoundName: "Sun", age: 17, gradeCertificate: [1, "B", 2, 1, undefined, undefined]};
+let listOfStudent: Student[] = [anton, paul, lisa];
+console.log(" ");
+console.log("++++++++++++++++++++++++++++++");
+printStudentList(listOfStudent);
