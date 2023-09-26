0a1eea74fa31233d702ff3febcc4505346fdaa25
UlrikeWerner <ulrike_werner@web.de>
2023-09-26 15:34:16 +0200

    task 2

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-type Grade = 1 | 2 | 3 | 4 | 5 | 6;
+type Grade = 1 | 2 | 3 | 4 | 5 | 6 | "A" | "B" | "C" | "D" | "E" | "F";
 
 type Student = {
     firstName: string
@@ -7,16 +7,20 @@ type Student = {
     gradeCertificate: Grade[]
 }
 
-let paul: Student = {firstName: "Paul", secoundName: "Peterson", age: 25, gradeCertificate: [1, 5, 4]}
+let anton: Student = {firstName: "Anton", secoundName: "Meier", age: 17, gradeCertificate: [1, 4, 3, 1, 3, 2, 1, 2]};
 
 function printStudent(student: Student): void {
+    console.log(" ");
     console.log(`${student.firstName} ${student.secoundName} (${student.age})`);
     let line: string = "";
-    for(let i = 0; i < 30; i++){
+    for(let i: number = 0; i < 30; i++){
         line += "=";
     }
     console.log(line);
     console.log(`Noten: ${student.gradeCertificate}`);
 }
 
-printStudent(paul);
+printStudent(anton);
+
+anton.gradeCertificate = ["A", 2, "F", 3, 1, "B", 2, 5];
+printStudent(anton);
